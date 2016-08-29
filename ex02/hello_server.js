a6de1800c2c4eec1816f59bdee139176256a80f9
andersonlemos <anderson.lemos@dmorj.com.br>
2016-08-29 17:27:52 -0300

    update files & create NTalk chat example

--- ex02/hello_server.js
+++ ex02/hello_server.js
@@ -1,7 +1,15 @@
 var http= require('http');
-var server = http.createServer(function(req,res){
+
+var atendeRequisicao = function(req,res){
     res.writeHead('200',{"Content-Type":"text-html"});
-    res.write('<h1>Hello World!</h1>');
+    res.write('<h1>Hello World!!</h1>');
     res.end();
-});
-server.listen(3000);
+};
+
+var server = http.createServer(atendeRequisicao);
+
+var servidorLigou =  function(){
+  console.log("Servidor rodando!");
+};
+
+server.listen(3001,servidorLigou);
