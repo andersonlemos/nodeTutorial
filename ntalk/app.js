a229958110f8ab41f4b0ab27f46e8692a556e918
andersonlemos <anderson.lemos@dmorj.com.br>
2016-08-31 17:36:10 -0300

    update

--- ntalk/app.js
+++ ntalk/app.js
@@ -1,14 +1,23 @@
 var express = require('express');
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var load = require('express-load');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('views',__dirname + '/views');
 app.set('view engine','ejs');
 
+app.use(cookieParser('ntalk'));
+app.use(expressSession());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + '/public'));
-app.use('/',routes);
-app.use('/usuarios',users);
+
+load('models')
+    .then('controllers')
+    .then('routes')
+    .into(app);
 
 app.listen(3000,function(){
   console.log('NTalk no ar');
